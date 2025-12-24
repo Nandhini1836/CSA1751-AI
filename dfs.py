706ff775009d51dea4aa2b7e38b8f420eea2a567
Nandhini1836 <192472318.simats@saveetha.com>
2025-12-24 08:58:13 +0530

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -1,13 +1,69 @@
-def minimax(depth, node, is_max, values):
-    if depth == 2:
-        return values[node]
+# DFS FUNCTION (Recursive)
+def dfs(graph, start):
+    visited = set()
+    result = []
 
-    if is_max:
-        return max(minimax(depth+1, node*2, False, values),
-                   minimax(depth+1, node*2+1, False, values))
-    else:
-        return min(minimax(depth+1, node*2, True, values),
-                   minimax(depth+1, node*2+1, True, values))
+    def dfs_visit(node):
+        if node not in visited:
+            visited.add(node)
+            result.append(node)
+            for neighbor in graph[node]:
+                dfs_visit(neighbor)
 
-values = [3, 5, 2, 9]
-print("Optimal Value:", minimax(0, 0, True, values))
+    dfs_visit(start)
+    return result
+
+
+# ---------------- EXAMPLE 1 ----------------
+graph1 = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': [],
+    'F': []
+}
+
+print("Example 1 DFS:", dfs(graph1, 'A'))
+
+
+# ---------------- EXAMPLE 2 ----------------
+graph2 = {
+    1: [2, 7],
+    2: [3, 6],
+    3: [4, 5],
+    4: [],
+    5: [],
+    6: [],
+    7: [8, 10],
+    8: [9],
+    9: [],
+    10: []
+}
+
+print("Example 2 DFS:", dfs(graph2, 1))
+
+
+# ---------------- EXAMPLE 3 ----------------
+graph3 = {
+    1: [2],
+    2: [3],
+    3: [4],
+    4: [5],
+    5: []
+}
+
+print("Example 3 DFS:", dfs(graph3, 1))
+
+
+# ---------------- EXAMPLE 4 ----------------
+graph4 = {
+    'X': ['Y', 'Z'],
+    'Y': ['P', 'Q'],
+    'Z': ['R'],
+    'P': [],
+    'Q': [],
+    'R': []
+}
+
+print("Example 4 DFS:", dfs(graph4, 'X'))
