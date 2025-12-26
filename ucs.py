62111d454366972e0a7fccb73bfbaf39742812d4
Nandhini1836 <192472318.simats@saveetha.com>
2025-12-26 13:46:34 +0530

    Update ucs.py

--- ucs.py
+++ ucs.py
@@ -1,41 +1,45 @@
 import heapq
 
-def uniform_cost_search(graph, start, goal):
-    pq = []
-    heapq.heappush(pq, (0, start, [start]))
+def UCS_Sum1():
+    graph = {
+        'S': [('A', 1), ('G', 12)],
+        'A': [('B', 3), ('C', 1)],
+        'B': [('D', 3)],
+        'C': [('D', 1), ('G', 2)],
+        'D': [('G', 3)],
+        'G': []
+    }
 
-    visited = set()
+    start = 'S'
+    goal = 'G'
+
+    pq = [(0, start)]
+    cost_so_far = {'S': 0}
+    parent = {'S': None}
 
     while pq:
-        cost, current, path = heapq.heappop(pq)
+        cost, node = heapq.heappop(pq)
 
-        if current == goal:
-            print("Path found:", " -> ".join(path))
-            print("Total cost:", cost)
-            return
+        if node == goal:
+            break
 
-        if current in visited:
-            continue
+        for neighbor, weight in graph[node]:
+            new_cost = cost + weight
+            if neighbor not in cost_so_far or new_cost < cost_so_far[neighbor]:
+                cost_so_far[neighbor] = new_cost
+                parent[neighbor] = node
+                heapq.heappush(pq, (new_cost, neighbor))
 
-        visited.add(current)
+    # Path reconstruction
+    path = []
+    n = goal
+    while n:
+        path.append(n)
+        n = parent[n]
+    path.reverse()
 
-        for neighbor, edge_cost in graph[current]:
-            if neighbor not in visited:
-                heapq.heappush(
-                    pq,
-                    (cost + edge_cost, neighbor, path + [neighbor])
-                )
-
-    print("No path found")
+    print("Path:", path)
+    print("Total Cost:", cost_so_far[goal])
 
 
-# -------- GRAPH --------
-graph = {
-    'A': [('B', 1), ('C', 4)],
-    'B': [('C', 2), ('D', 5)],
-    'C': [('D', 1)],
-    'D': []
-}
-
-# -------- FUNCTION CALL (THIS IS REQUIRED) --------
-uniform_cost_search(graph, 'A', 'D')
+UCS_Sum1()
